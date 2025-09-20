2f47e44401851f343306d73ba2feb592b3ffb72d
Hrutik Chaudhari <hrutikchaudhari05@gmail.com>
2025-09-20 17:59:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
-    //Write your code here
 
+	const fnameInput = document.querySelector('input[name="fname"]').value.trim();
+	const lnameInput = document.querySelector('input[name="lname"]').value.trim();
+	const btn = document.querySelector('input[value="Submit"]'); 
+
+	alert(`${fnameInput} ${lnameInput}`);
 }
